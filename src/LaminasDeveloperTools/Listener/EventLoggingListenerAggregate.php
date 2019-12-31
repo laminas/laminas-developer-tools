16a6cdaa60d51367b3667618539156d4f907eb1e
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:39:53 -0600

    Rewriting as Laminas Project package

---
+++ src/LaminasDeveloperTools/Listener/EventLoggingListenerAggregate.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * @see       https://github.com/laminas/laminas-developer-tools for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-developer-tools/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-developer-tools/blob/master/LICENSE.md New BSD License
+ */
+
+namespace Laminas\DeveloperTools\Listener;
+
+use Laminas\DeveloperTools\Collector\CollectorInterface;
+use Laminas\DeveloperTools\Profiler;
+use Laminas\EventManager\Event;
+use Laminas\EventManager\SharedEventManagerInterface;
+use Laminas\EventManager\SharedListenerAggregateInterface;
+use Laminas\ServiceManager\Exception\ServiceNotFoundException;
+
+/**
+ * Listens to defined events to allow event-level collection of statistics.
+ *
+ * @author Mark Garrett <mark@moderndeveloperllc.com>
+ * @since 0.0.3
+ */
+class EventLoggingListenerAggregate implements SharedListenerAggregateInterface
+{
+    /**
+     * @var \Laminas\DeveloperTools\Collector\EventCollectorInterface[]
+     */
+    protected $collectors;
+
+    /**
+     * @var The event identifiers to collect
+     */
+    protected $identifiers;
+
+    /**
+     * Constructor.
+     *
+     * @param \Laminas\DeveloperTools\Collector\EventCollectorInterface[] $collectors
+     * @param string[]                                                $identifiers
+     */
+    public function __construct(array $collectors, array $identifiers)
+    {
+        $this->collectors = array_map(
+            function (CollectorInterface $collector) {
+                return $collector;
+            },
+            $collectors
+        );
+        $this->identifiers = array_values(array_map(
+            function ($identifier) {
+                return (string) $identifier;
+            },
+            $identifiers
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attachShared(SharedEventManagerInterface $events)
+    {
+        $events->attach($this->identifiers, '*', array($this,'onCollectEvent'), Profiler::PRIORITY_EVENT_COLLECTOR);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function detachShared(SharedEventManagerInterface $events)
+    {
+        // can't be detached
+    }
+
+    /**
+     * Callback to process events
+     *
+     * @param Event $event
+     *
+     * @return bool
+     *
+     * @throws ServiceNotFoundException
+     */
+    public function onCollectEvent(Event $event)
+    {
+        foreach ($this->collectors as $collector) {
+            $collector->collectEvent('application', $event);
+        }
+
+        // @TODO workaround, to be removed when https://github.com/zendframework/zf2/pull/6147 is fixed
+        // Returns a "truthy" value, but not boolean true, to ensure that cases such as https://github.com/zfcampus/zf-apigility/issues/56
+        // continue to work correctly as well.
+        return 1;
+    }
+}
---
+++ src/LaminasDeveloperTools/View/Helper/DetailArray.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @see       https://github.com/laminas/laminas-developer-tools for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-developer-tools/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-developer-tools/blob/master/LICENSE.md New BSD License
+ */
+
+namespace Laminas\DeveloperTools\View\Helper;
+
+use Laminas\View\Helper\AbstractHelper;
+
+/**
+ * @copyright  Copyright (c) 2005-2014 Laminas (https://www.zend.com)
+ * @license    https://getlaminas.org/license/new-bsd     New BSD License
+ */
+class DetailArray extends AbstractHelper
+{
+    /**
+     * Renders a detail entry for an array.
+     *
+     * @param  string  $label Label name
+     * @param  array   $details Value array (list)
+     * @param  bool $redundant Marks this detail as redundant.
+     * @return string
+     */
+    public function __invoke($label, array $details, $redundant = false)
+    {
+        $r = array();
+
+        $r[] = '<span class="laminas-toolbar-info';
+        $r[] = ($redundant) ? ' laminas-toolbar-info-redundant' : '';
+        $r[] = '">';
+
+        $r[] = '<span class="laminas-detail-label">';
+        $r[] = $label;
+        $r[] = '</span>';
+
+
+        $extraCss = '';
+        $newLine  = false;
+
+        foreach ($details as $entry) {
+            if ($newLine === true) {
+                $r[] = '</span><span class="laminas-toolbar-info';
+                $r[] = ($redundant) ? ' laminas-toolbar-info-redundant' : '';
+                $r[] = '">';
+            }
+
+            $r[] = sprintf('<span class="laminas-detail-value%s">%s</span>', $extraCss, $entry);
+
+            $newLine  = true;
+            $extraCss = ' laminas-detail-extra-value';
+        }
+
+        $r[] = '</span>';
+
+        return implode('', $r);
+    }
+}
