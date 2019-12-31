01a2f68a93a7529da11c70fa92f33ac36903effa
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:39:52 -0600

    Rewriting as Laminas Project package

---
+++ src/LaminasDeveloperTools/Collector/AutoHideInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @see       https://github.com/laminas/laminas-developer-tools for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-developer-tools/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-developer-tools/blob/master/LICENSE.md New BSD License
+ */
+
+namespace Laminas\DeveloperTools\Collector;
+
+/**
+ * Auto hide Interface provides the ability for collectors, to specify that
+ * they can be hidden.
+ *
+ * @copyright  Copyright (c) 2005-2014 Laminas (https://www.zend.com)
+ * @license    https://getlaminas.org/license/new-bsd     New BSD License
+ */
+interface AutoHideInterface
+{
+    /**
+     * Returns true if the collector can be hidden, because it is empty.
+     *
+     * @return bool
+     */
+    public function canHide();
+}
---
+++ src/LaminasDeveloperTools/Collector/EventCollectorInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @see       https://github.com/laminas/laminas-developer-tools for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-developer-tools/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-developer-tools/blob/master/LICENSE.md New BSD License
+ */
+
+namespace Laminas\DeveloperTools\Collector;
+
+use Laminas\EventManager\Event;
+
+/**
+ * Event Data Collector Interface.
+ *
+ */
+interface EventCollectorInterface
+{
+    /**
+     * Collects event-level information
+     *
+     * @param string $id
+     * @param Event  $event
+     */
+    public function collectEvent($id, Event $event);
+}
