d6e9bd3bcc91b55264ab22f28c8bb346338c850a
Andi Rückauer <arueckauer@gmail.com>
2021-09-30 09:21:14 +0200

    Remove file headers
    Signed-off-by: Andi Rückauer <arueckauer@gmail.com>

--- src/Listener/FirePhpListener.php
+++ src/Listener/FirePhpListener.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-developer-tools for the canonical source repository
- * @copyright https://github.com/laminas/laminas-developer-tools/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-developer-tools/blob/master/LICENSE.md New BSD License
- */
-
 namespace Laminas\DeveloperTools\Listener;
 
 use Laminas\DeveloperTools\Profiler;
